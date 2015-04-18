afecc72578ede71e1cabea89ffcaa113004871e8
Chris Davies <chris.davies@theironyard.com>
2015-04-18 14:16:36 -0400

    Tidying up the dom

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,6 +1,34 @@
 app('dom', function () {
-  var dom = function (selector) {
+  return function (selector) {
     var arr = Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+
+    function setProp(prop, val) {
+      if (val === undefined) return arr[0] ? arr[0][prop] : undefined;
+
+      return self.forEach(function (el) {
+        el[prop] = val;
+      });
+    }
+
+    function classes (el) {
+      return (el.getAttribute('class') || '').split(/[^\w\-]+/g);
+    }
+
+    function hasClass (el, className) {
+      return classes(el).indexOf(className) >= 0;
+    }
+
+    function removeClass (className, el) {
+      el.setAttribute('class', classes(el).filter(function (cls) {
+        return cls !== className;
+      }).join(' '));
+    }
+
+    function addClass (className, el) {
+      if (!hasClass(el, className)) {
+        el.setAttribute('class', el.getAttribute('class') + ' ' + className);
+      }
+    }
 
     var self = {
       length: arr.length,
@@ -16,74 +44,44 @@ app('dom', function () {
 
       hasClass: function (cls) {
         return arr.some(function (el) {
-          hasClass(el, cls);
+          return hasClass(el, cls);
         });
       },
 
       removeClass: function (cls) {
-        arr.forEach(function (el) {
-          removeClass(el, cls)
-        });
-
-        return self;
+        return self.forEach(removeClass.bind(null, cls));
       },
 
       addClass: function (cls) {
-        arr.forEach(function (el) {
-          addClass(el, cls)
-        });
-
-        return self;
+        return self.forEach(addClass.bind(null, cls));
       },
 
       css: function (def) {
-        arr.forEach(function (el) {
+        return self.forEach(function (el) {
           for (var prop in def) {
             el.style[prop] = def[prop];
           }
         });
-
-        return self;
       },
 
       text: function (txt) {
-        if (txt === undefined) {
-          return arr[0] ? arr[0].textContent : undefined;
-        }
-
-        arr.forEach(function (el) {
-          el.textContent = txt;
-        });
-
-        return self;
+        return setProp('textContent', txt);
       },
 
       val: function (txt) {
-        if (txt === undefined) {
-          return arr[0] ? arr[0].value : undefined;
-        }
-
-        arr.forEach(function (el) {
-          el.value = txt;
-        });
-
-        return self;
+        return setProp('value', txt);
       },
 
       on: function (evt, handler) {
-        arr.forEach(function (el) {
+        return self.forEach(function (el) {
           el.addEventListener(evt, handler);
         });
-
-        return self;
       },
 
       off: function (evt, handler) {
-        arr.forEach(function (el) {
+        return self.forEach(function (el) {
           el.removeEventListener(evt, handler);
         });
-
-        return self;
       },
 
       append: function (html) {
@@ -96,33 +94,13 @@ app('dom', function () {
 
       select: function () {
         var el = arr[0];
+
         el && typeof(el.select) === 'function' && el.select();
+
         return self;
       }
     };
 
     return self;
   };
-
-  function classes (el) {
-    return (el.getAttribute('class') || '').split(/[^\w\-]+/g);
-  }
-
-  function hasClass (el, className) {
-    return classes(el).indexOf(className) >= 0;
-  }
-
-  function removeClass (el, className) {
-    el.setAttribute('class', classes(el).filter(function (cls) {
-      return cls !== className;
-    }).join(' '));
-  }
-
-  function addClass (el, className) {
-    if (!hasClass(el, className)) {
-      el.setAttribute('class', el.getAttribute('class') + ' ' + className);
-    }
-  }
-
-  return dom;
 });
