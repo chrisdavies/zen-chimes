5803b31ca5e9bb325191bd8c339beed85523b1c2
Chris Davies <chris.davies@theironyard.com>
2015-04-18 12:40:34 -0400

    Making edit a form

--- src/js/init.js
+++ src/js/init.js
@@ -15,7 +15,8 @@
   var timeInput = dom.one('input[name=time]');
   timeInput.value = settings.minutes;
 
-  dom.one('.btn-save').addEventListener('click', function () {
+  dom.one('.settings-form').addEventListener('submit', function (e) {
+    e.preventDefault();
     settings.minutes = parseInt(timeInput.value) || 10;
     settingsStore.update(settings);
     page.show('playing-page');
