f0507a93a27c75a3f8e2c708825e70feda1d68c9
Chris Davies <chris.davies@theironyard.com>
2015-04-18 15:32:55 -0400

    Adding firefox notice

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Zen Chimes
 
 Relax. Listen to chimes as you meditate. The chimes stop when the timer stops. No jarring alarms to break your meditation.
+
+## Issues...
+
+- FireFox is unsupported for now. It has issues with multiple playback.
--- src/js/load-audio.js
+++ src/js/load-audio.js
@@ -1,7 +1,23 @@
 app('load-audio', function () {
   return function (done) {
-    var $ = app('dom'),
-        loadCount = 0,
+    var $ = app('dom');
+
+    if (isFirefox()) {
+      showUnsupportedMessage();
+    } else {
+      loadAudio();
+    }
+
+    function isFirefox() {
+      return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+    }
+
+    function showUnsupportedMessage() {
+      $('.loading-message').text('FireFox is unsupported. :/');
+    }
+
+    function loadAudio() {
+      var loadCount = 0,
         wav = [
           'c1',
           'c2',
@@ -10,36 +26,37 @@ app('load-audio', function () {
           'c5',
           'c6'
         ];
+      
+      var audioHtml = wav.map(function (w) {
+        return '<audio preload="auto">' +
+          '<source src="audio/' + w + '.mp3" />' +
+          '<source src="audio/' + w + '.wav" />' +
+          '</audio>';
+      }).join('');
 
-    var audioHtml = wav.map(function (w) {
-      return '<audio preload="auto">' +
-        '<source src="audio/' + w + '.mp3" />' +
-        '<source src="audio/' + w + '.wav" />' +
-        '</audio>';
-    }).join('');
+      var bar = $('.loading-bar');
 
-    var bar = $('.loading-bar');
+      $('.zen-page').append(audioHtml);
 
-    $('.zen-page').append(audioHtml);
+      function incIfReady (audio) {
+        if (audio.readyState <= 3 || loadCount >= wav.length) return false;
 
-    function incIfReady (audio) {
-      if (audio.readyState <= 3 || loadCount >= wav.length) return false;
+        ++loadCount;
 
-      ++loadCount;
+        bar.css({
+          width: ((loadCount / wav.length) * 100) + '%'
+        });
 
-      bar.css({
-        width: ((loadCount / wav.length) * 100) + '%'
+        (loadCount >= wav.length) && done && done();
+
+        return true;
+      }
+
+      $('audio').on('canplay', function (e) {
+        incIfReady(e.target);
+      }).forEach(function (audio) {
+        audio.load();
       });
-
-      (loadCount >= wav.length) && done && done();
-
-      return true;
     }
-
-    $('audio').on('canplay', function (e) {
-      incIfReady(e.target);
-    }).forEach(function (audio) {
-      audio.load();
-    });
   };
 });
